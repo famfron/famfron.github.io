d7bc9d6728f1d289d1824d23b8d4017e31c0612f
famfron <famfron@gmail.com>
2020-02-03 23:00:25 -0700

    Initial Upload of Current Files
    The first upload of all files that are completed at the time. Many more to come, but I wanted to at the very least get the website up and running.

---
+++ do_things.js
@@ -0,0 +1,99 @@
+function blog_page() {
+    window.location.href = "blog.html"
+}
+
+function home_page() {
+    window.location.href = "index.html"
+}
+
+function photo_page() {
+    window.location.href = "photos.html"
+}
+
+function music_page() {
+    window.location.href = "music.html"
+}
+
+function jan_photos() {
+    window.location.href = "photos_months/jan_photos.html"
+}
+
+function feb_photos() {
+    window.location.href = "photos_months/feb_photos.html"
+}
+
+function mar_photos() {
+    window.location.href = "photos_months/mar_photos.html"
+}
+
+function apr_photos() {
+    window.location.href = "photos_months/apr_photos.html"
+}
+
+function may_photos() {
+    window.location.href = "photos_months/may_photos.html"
+}
+
+function jun_photos() {
+    window.location.href = "photos_months/jun_photos.html"
+}
+
+function jul_photos() {
+    window.location.href = "photos_months/jul_photos.html"
+}
+
+function aug_photos() {
+    window.location.href = "photos_months/aug_photos.html"
+}
+
+function sep_photos() {
+    window.location.href = "photos_months/sep_photos.html"
+}
+
+function oct_photos() {
+    window.location.href = "photos_months/oct_photos.html"
+}
+
+function nov_photos() {
+    window.location.href = "photos_months/nov_photos.html"
+}
+
+function dec_photos() {
+    window.location.href = "photos_months/dec_photos.html"
+}
+
+function music_2012() {
+    window.location.href = "music_years/2012_music.html"
+}
+
+function music_2013() {
+    window.location.href = "music_years/2013_music.html"
+}
+
+function music_2014() {
+    window.location.href = "music_years/2014_music.html"
+}
+
+function music_2015() {
+    window.location.href = "music_years/2015_music.html"
+}
+
+function music_2016() {
+    window.location.href = "music_years/2016_music.html"
+}
+
+function music_2017() {
+    window.location.href = "music_years/2017_music.html"
+}
+
+function music_2018() {
+    window.location.href = "music_years/2018_music.html"
+}
+
+function music_2019() {
+    window.location.href = "music_years/2019_music.html"
+}
+
+function music_2020() {
+    window.location.href = "music_years/2020_music.html"
+}
