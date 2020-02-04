35ec3521a272ddca5ee2c8d7b81ef8c666aada61
famfron <famfron@gmail.com>
2020-02-04 13:25:53 -0700

    Added .htacces
    Attempting to remove the .html from the end of the site. Also cleaned up the home page

--- do_things.js
+++ do_things.js
@@ -1,123 +1,123 @@
 function blog_page() {
-    window.location.href = "blog.html"
+    window.location.href = "blog"
 }
 
 function home_page() {
-    window.location.href = "home_page.html"
+    window.location.href = "home_page"
 }
 
 function photo_page() {
-    window.location.href = "photos.html"
+    window.location.href = "photos"
 }
 
 function music_page() {
-    window.location.href = "music.html"
+    window.location.href = "music"
 }
 
 function inDevelopment() {
-    window.location.href = "page_inDevelopment.html"
+    window.location.href = "page_inDevelopment"
 }
 
 function inDevelopment_parent() {
-    window.location.href = "../page_inDevelopment.html"
+    window.location.href = "../page_inDevelopment"
 }
 
 function home_page_parent() {
-    window.location.href = "../home_page.html"
+    window.location.href = "../home_page "
 }
 
 function blog_page_parent() {
-    window.location.href = "../blog.html"
+    window.location.href = "../blog "
 }
 
 function photo_page_parent() {
-    window.location.href = "../photos.html"
+    window.location.href = "../photos "
 }
 
 function music_page_parent() {
-    window.location.href = "../music.html"
+    window.location.href = "../music "
 }
 
 function jan_photos() {
-    window.location.href = "photos_months/jan_photos.html"
+    window.location.href = "photos_months/jan_photos "
 }
 
 function feb_photos() {
-    window.location.href = "photos_months/feb_photos.html"
+    window.location.href = "photos_months/feb_photos "
 }
 
 function mar_photos() {
-    window.location.href = "photos_months/mar_photos.html"
+    window.location.href = "photos_months/mar_photos "
 }
 
 function apr_photos() {
-    window.location.href = "photos_months/apr_photos.html"
+    window.location.href = "photos_months/apr_photos "
 }
 
 function may_photos() {
-    window.location.href = "photos_months/may_photos.html"
+    window.location.href = "photos_months/may_photos "
 }
 
 function jun_photos() {
-    window.location.href = "photos_months/jun_photos.html"
+    window.location.href = "photos_months/jun_photos "
 }
 
 function jul_photos() {
-    window.location.href = "photos_months/jul_photos.html"
+    window.location.href = "photos_months/jul_photos "
 }
 
 function aug_photos() {
-    window.location.href = "photos_months/aug_photos.html"
+    window.location.href = "photos_months/aug_photos "
 }
 
 function sep_photos() {
-    window.location.href = "photos_months/sep_photos.html"
+    window.location.href = "photos_months/sep_photos "
 }
 
 function oct_photos() {
-    window.location.href = "photos_months/oct_photos.html"
+    window.location.href = "photos_months/oct_photos "
 }
 
 function nov_photos() {
-    window.location.href = "photos_months/nov_photos.html"
+    window.location.href = "photos_months/nov_photos "
 }
 
 function dec_photos() {
-    window.location.href = "photos_months/dec_photos.html"
+    window.location.href = "photos_months/dec_photos "
 }
 
 function music_2012() {
-    window.location.href = "music_years/2012_music.html"
+    window.location.href = "music_years/2012_music "
 }
 
 function music_2013() {
-    window.location.href = "music_years/2013_music.html"
+    window.location.href = "music_years/2013_music "
 }
 
 function music_2014() {
-    window.location.href = "music_years/2014_music.html"
+    window.location.href = "music_years/2014_music "
 }
 
 function music_2015() {
-    window.location.href = "music_years/2015_music.html"
+    window.location.href = "music_years/2015_music "
 }
 
 function music_2016() {
-    window.location.href = "music_years/2016_music.html"
+    window.location.href = "music_years/2016_music "
 }
 
 function music_2017() {
-    window.location.href = "music_years/2017_music.html"
+    window.location.href = "music_years/2017_music "
 }
 
 function music_2018() {
-    window.location.href = "music_years/2018_music.html"
+    window.location.href = "music_years/2018_music "
 }
 
 function music_2019() {
-    window.location.href = "music_years/2019_music.html"
+    window.location.href = "music_years/2019_music "
 }
 
 function music_2020() {
-    window.location.href = "music_years/2020_music.html"
+    window.location.href = "music_years/2020_music "
 }
