05e47713b243f4ddf33a2c4e1771cdc3a31665e1
famfron <famfron@gmail.com>
2020-02-04 15:28:54 -0700

    v1.03
    Removed .htaccess, minor adjustments to do things and page development to get them working properly. Started work on template for all future blog entries

--- do_things.js
+++ do_things.js
@@ -23,101 +23,101 @@ function inDevelopment_parent() {
 }
 
 function home_page_parent() {
-    window.location.href = "../home_page "
+    window.location.href = "../home_page"
 }
 
 function blog_page_parent() {
-    window.location.href = "../blog "
+    window.location.href = "../blog"
 }
 
 function photo_page_parent() {
-    window.location.href = "../photos "
+    window.location.href = "../photos"
 }
 
 function music_page_parent() {
-    window.location.href = "../music "
+    window.location.href = "../music"
 }
 
 function jan_photos() {
-    window.location.href = "photos_months/jan_photos "
+    window.location.href = "photos_months/jan_photos"
 }
 
 function feb_photos() {
-    window.location.href = "photos_months/feb_photos "
+    window.location.href = "photos_months/feb_photos"
 }
 
 function mar_photos() {
-    window.location.href = "photos_months/mar_photos "
+    window.location.href = "photos_months/mar_photos"
 }
 
 function apr_photos() {
-    window.location.href = "photos_months/apr_photos "
+    window.location.href = "photos_months/apr_photos"
 }
 
 function may_photos() {
-    window.location.href = "photos_months/may_photos "
+    window.location.href = "photos_months/may_photos"
 }
 
 function jun_photos() {
-    window.location.href = "photos_months/jun_photos "
+    window.location.href = "photos_months/jun_photos"
 }
 
 function jul_photos() {
-    window.location.href = "photos_months/jul_photos "
+    window.location.href = "photos_months/jul_photos"
 }
 
 function aug_photos() {
-    window.location.href = "photos_months/aug_photos "
+    window.location.href = "photos_months/aug_photos"
 }
 
 function sep_photos() {
-    window.location.href = "photos_months/sep_photos "
+    window.location.href = "photos_months/sep_photos"
 }
 
 function oct_photos() {
-    window.location.href = "photos_months/oct_photos "
+    window.location.href = "photos_months/oct_photos"
 }
 
 function nov_photos() {
-    window.location.href = "photos_months/nov_photos "
+    window.location.href = "photos_months/nov_photos"
 }
 
 function dec_photos() {
-    window.location.href = "photos_months/dec_photos "
+    window.location.href = "photos_months/dec_photos"
 }
 
 function music_2012() {
-    window.location.href = "music_years/2012_music "
+    window.location.href = "music_years/2012_music"
 }
 
 function music_2013() {
-    window.location.href = "music_years/2013_music "
+    window.location.href = "music_years/2013_music"
 }
 
 function music_2014() {
-    window.location.href = "music_years/2014_music "
+    window.location.href = "music_years/2014_music"
 }
 
 function music_2015() {
-    window.location.href = "music_years/2015_music "
+    window.location.href = "music_years/2015_music"
 }
 
 function music_2016() {
-    window.location.href = "music_years/2016_music "
+    window.location.href = "music_years/2016_music"
 }
 
 function music_2017() {
-    window.location.href = "music_years/2017_music "
+    window.location.href = "music_years/2017_music"
 }
 
 function music_2018() {
-    window.location.href = "music_years/2018_music "
+    window.location.href = "music_years/2018_music"
 }
 
 function music_2019() {
-    window.location.href = "music_years/2019_music "
+    window.location.href = "music_years/2019_music"
 }
 
 function music_2020() {
-    window.location.href = "music_years/2020_music "
+    window.location.href = "music_years/2020_music"
 }
